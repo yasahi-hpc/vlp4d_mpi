44d86bb69dd5631569e4477e8f54c0622a253ef5
Yuuichi ASAHI <asahi.yuuichi@qst.go.jp>
2020-09-17 10:38:59 +0900

    Removed unused initialization in Poisson solver

--- src_kokkos/Field.cpp
+++ src_kokkos/Field.cpp
@@ -5,41 +5,12 @@ void lu_solve_poisson(Config *conf, Efield *ef, Diags *dg, int iter);
 
 void field_rho(Config *conf, RealOffsetView4D fn, Efield *ef, const std::vector<int> &tiles) {
   const Domain *dom = &(conf->dom_);
-
-  int nx = dom->nxmax_[0], ny = dom->nxmax_[1];
-  int nvx = dom->nxmax_[2], nvy = dom->nxmax_[3];
   float64 dvx = dom->dx_[2], dvy = dom->dx_[3];
-
-  // Capturing a class member causes a problem
-  // See https://github.com/kokkos/kokkos/issues/695
-  RealView2D rho     = ef->rho_;
-  RealView2D rho_loc = ef->rho_loc_;
-  RealView2D ex      = ef->ex_;
-  RealView2D ey      = ef->ey_;
-  RealView2D phi     = ef->phi_;
-
-  MDPolicyType_2D initialization_policy2d({{0, 0}},
-                                          {{nx, ny}},
-                                          {{BASIC_TILE_SIZE0, BASIC_TILE_SIZE1}}
-                                         );
-
-  Kokkos::parallel_for("initialization", initialization_policy2d, KOKKOS_LAMBDA (const int ix, const int iy) {
-    rho_loc(ix, iy) = 0.;
-    ex(ix, iy)      = 0.;
-    ey(ix, iy)      = 0.;
-    phi(ix, iy)     = 0.;
-  });
-
-  int nx_loc  = dom->local_nx_[0];
-  int ny_loc  = dom->local_nx_[1];
-  int nvx_loc = dom->local_nx_[2];
-  int nvy_loc = dom->local_nx_[3];
-  int local_xstart  = dom->local_nxmin_[0];
-  int local_ystart  = dom->local_nxmin_[1];
-  int local_vxstart = dom->local_nxmin_[2];
-  int local_vystart = dom->local_nxmin_[3];
   int nx_min = dom->local_nxmin_[0], ny_min = dom->local_nxmin_[1], nvx_min = dom->local_nxmin_[2], nvy_min = dom->local_nxmin_[3];
   int nx_max = dom->local_nxmax_[0] + 1, ny_max = dom->local_nxmax_[1] + 1, nvx_max = dom->local_nxmax_[2] + 1, nvy_max = dom->local_nxmax_[3] + 1;
+
+  // In order to capture a class member by lambda
+  RealView2D rho_loc = ef->rho_loc_;
 
   const int TX = tiles[0], TY = tiles[1];
   MDPolicyType_2D integral_policy2d({{nx_min, ny_min}},
@@ -54,7 +25,6 @@ void field_rho(Config *conf, RealOffsetView4D fn, Efield *ef, const std::vector<
         sum += fn(ix, iy, ivx, ivy);
       }
     }
-
     // stored in the global address
     rho_loc(ix, iy) = sum * dvx * dvy;
   });
