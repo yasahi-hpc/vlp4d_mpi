4b69d2482d4b9a3a028695a2c8b12876a42dbba7
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2021-02-02 18:15:04 +0900

    [Porting s86] Kokkos version ported

---
+++ wk/batch_scripts/sub_s86_v100_kokkos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -q pg9
+#PBS -l select=2:ncpus=48:mpiprocs=4:ompthreads=12:ngpus=4
+#PBS -l walltime=00:10:00
+#PBS -P vlp4d
+
+cd $PBS_O_WORKDIR
+. /etc/profile.d/modules.sh
+
+module purge
+module load gcc/7.4.0
+module load intel/20.0.2
+module load cuda/11.0
+module load mpt/2.23-ga
+
+export MPI_USE_CUDA=1
+
+mpirun -np 8 ./vlp4d.s86_v100_kokkos --num_threads 1 --teams 1 --device 0 --num_gpus 4 --device_map 1 -f SLD10.dat
