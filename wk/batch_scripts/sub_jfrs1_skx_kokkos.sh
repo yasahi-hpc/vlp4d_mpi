c18842b8544fc4473d61896f7f622029bb6e1806
Yuuichi ASAHI <asahi.yuuichi@qst.go.jp>
2021-02-17 13:50:36 +0900

    [PORT JFRS1] job script

--- wk/batch_scripts/sub_jfrs1_skx_kokkos.sh
+++ wk/batch_scripts/sub_jfrs1_skx_kokkos.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH -J omp               # jobname
-#SBATCH --nodes=2            # Number of nodes
+#SBATCH --nodes=4            # Number of nodes
 #SBATCH --ntasks-per-node=4  # Number of processes per node
 #SBATCH --cpus-per-task=20   # logical core per task
 #SBATCH --time=00:30:00      # execute time (hh:mm:ss)
 #SBATCH --account=GT5DSTC    # account number
 #SBATCH -o %j.out            # strout filename (%j is jobid)
 #SBATCH -e %j.err            # stderr filename (%j is jobid)
-#SBATCH -p dev               # Job class
+#SBATCH -p S-M               # Job class
 
 source /opt/modules/default/init/bash
 module switch PrgEnv-intel PrgEnv-gnu
